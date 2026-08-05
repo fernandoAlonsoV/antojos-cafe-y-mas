b1cc6e0f311506d753b044f12baba02801db22e8
Fernando Alonso <luis.fernando.alonso.velazquez@gmail.com>
2026-08-05 15:55:47 -0600

    commented Qr component

--- src/components/Qr.tsx
+++ src/components/Qr.tsx
@@ -1,59 +1,59 @@
-import QRCodeStyling from "qr-code-styling";
-import { useEffect, useRef } from "react";
+// import QRCodeStyling from "qr-code-styling";
+// import { useEffect, useRef } from "react";
 
-const qrCode = new QRCodeStyling({
-  width: 1024,
-  height: 1024,
-  type: "svg",
-  data: "https://antojos-cafe-y-mas.vercel.app/",
-  image: "/public/icons/logo.svg", // tu logo
-  dotsOptions: {
-    color: "#4B2E1E",
-    type: "rounded",
-  },
-  cornersSquareOptions: {
-    color: "#4B2E1E",
-    type: "extra-rounded",
-  },
-  cornersDotOptions: {
-    color: "#4B2E1E",
-    type: "dot",
-  },
-  backgroundOptions: {
-    color: "#FFFFFF",
-  },
-  imageOptions: {
-    crossOrigin: "anonymous",
-    margin: 12,
-    imageSize: 0.28,
-  },
-  qrOptions: {
-    errorCorrectionLevel: "H",
-  },
-});
+// const qrCode = new QRCodeStyling({
+//   width: 1024,
+//   height: 1024,
+//   type: "svg",
+//   data: "https://antojos-cafe-y-mas.vercel.app/",
+//   image: "/public/icons/logo.svg", // tu logo
+//   dotsOptions: {
+//     color: "#4B2E1E",
+//     type: "rounded",
+//   },
+//   cornersSquareOptions: {
+//     color: "#4B2E1E",
+//     type: "extra-rounded",
+//   },
+//   cornersDotOptions: {
+//     color: "#4B2E1E",
+//     type: "dot",
+//   },
+//   backgroundOptions: {
+//     color: "#FFFFFF",
+//   },
+//   imageOptions: {
+//     crossOrigin: "anonymous",
+//     margin: 12,
+//     imageSize: 0.28,
+//   },
+//   qrOptions: {
+//     errorCorrectionLevel: "H",
+//   },
+// });
 
-export default function QR() {
-  const ref = useRef<HTMLDivElement>(null);
+// export default function QR() {
+//   const ref = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    if (ref.current) {
-      qrCode.append(ref.current);
-    }
-  }, []);
+//   useEffect(() => {
+//     if (ref.current) {
+//       qrCode.append(ref.current);
+//     }
+//   }, []);
 
-  return (
-    <>
-      <div style={{ width: "40px" }} ref={ref} />
-      <button
-        onClick={() =>
-          qrCode.download({
-            extension: "svg",
-            name: "antojos-cafe",
-          })
-        }
-      >
-        Descargar SVG
-      </button>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <div style={{ width: "40px" }} ref={ref} />
+//       <button
+//         onClick={() =>
+//           qrCode.download({
+//             extension: "svg",
+//             name: "antojos-cafe",
+//           })
+//         }
+//       >
+//         Descargar SVG
+//       </button>
+//     </>
+//   );
+// }
